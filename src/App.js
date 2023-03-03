d4e0ab993c78dcadf607f56b587e29bffe104db1
Abhishekkanojiya3 <abhi67562@gmail.com>
2023-03-04 00:03:06 +0530

    building blocks for ecoomerce

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+//import { Button } from 'react-bootstrap';
+import CartItems from './components/CartItem/CartItem';
+import NavBar from './components/CartItem/NavBar/Navbar';
+
+function App() {
+    return ( <
+        >
+        <
+        NavBar / >
+        <
+        h1 class = "text-center p-5  bg-secondary text-white" > The Generics < /h1> <
+        CartItems / >
+
+
+        <
+        />
+    );
+}
+
+export default App;
